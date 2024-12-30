0728d002a783369541737623136412d1514f0b31
olligobber <sirolligobbervii@yahoo.com.au>
2024-12-30 11:21:26 +1100

    Fixed handling of usernames with spaces

--- WOM_Wiki_Overlap.sh
+++ WOM_Wiki_Overlap.sh
@@ -17,10 +17,13 @@ wget "http://api.wiseoldman.net/v2/groups/$womid/csv" -O womgroup.csv -q
 wget "https://www.f2p.wiki/clans/$wikiid" -O wikigroup.html -q
 
 # remove header row and get first column from wom csv file
-cat womgroup.csv | tail -n+2 | cut -d, -f1 | sort | tr '[:upper:]' '[:lower:]' | cat > womgroup
+cat womgroup.csv | tail -n+2 | cut -d, -f1 | \
+	sort | tr '[:upper:]' '[:lower:]' | cat > womgroup
 
 # look for all links to a player page from wiki page to get member names
-cat wikigroup.html | grep "players/" | sed "s|.*players/\([^\"]*\)\".*|\1|g" | \
+cat wikigroup.html | grep "<a href=\"/players/" | \
+	sed "s|.*<a href=\"/players/[^\"]*\">\([^<]*\)</a>.*|\1|g" | \
+	sed "s/\xc2\xa0/ /g" | \
 	sort | tr '[:upper:]' '[:lower:]' | cat > wikigroup
 
 # find the overlap
